92246bef450b85062e29236725951a4393916535
stefan.duadsko <stefan.dudasko@triad.sk>
2021-01-24 10:51:03 +0100

    Init

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        FollowCamera followCamera = GetComponent<FollowCamera>();
+
+        transform.position = target.position;
+
+
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Start()
+    {
+        // Start
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor() {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+
+            agent.destination = hit.point;
+        }
+    }
+
+}
