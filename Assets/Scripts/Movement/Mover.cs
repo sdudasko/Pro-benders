33136cbd5f79540bc1247a89d33aa3feb02ac1a9
stefan.duadsko <stefan.dudasko@triad.sk>
2021-02-07 20:47:58 +0100

    Finished saving system

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,4 +1,5 @@
-﻿using RPG.Combat;
+﻿using System.Collections.Generic;
+using RPG.Combat;
 using RPG.Core;
 using RPG.Saving;
 using UnityEngine;
@@ -54,17 +55,30 @@ namespace RPG.Movement
             MoveTo(destination, speedFraction);
         }
 
+        [System.Serializable] struct MoverSaveData
+        {
+            public SerializableVector3 position;
+            public SerializableVector3 rotation;
+        }
+
         public object CaptureState()
         {
-            return new SerializableVector3(transform.position);
+            MoverSaveData data = new MoverSaveData();
+            data.position = new SerializableVector3(transform.position);
+            data.rotation = new SerializableVector3(transform.eulerAngles);
+
+            return data;
         }
 
         public void RestoreState(object state)
         {
-            SerializableVector3 position = (SerializableVector3)state;
+            MoverSaveData data = (MoverSaveData)state;
 
             GetComponent<NavMeshAgent>().enabled = false;
-            transform.position = position.ToVector();
+
+            transform.position = data.position.ToVector();
+            transform.eulerAngles = data.rotation.ToVector();
+
             GetComponent<NavMeshAgent>().enabled = true;
         }
     }
